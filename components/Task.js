640f606eec6577e16b1705d217755a43f193bb3e
miyamoto sigeharu <sigeharumiyamoto@gmail.com>
2021-07-11 21:48:33 +0900

    first commit

---
+++ components/Task.js
@@ -0,0 +1,66 @@
+import Link from "next/link";
+import Cookie from "universal-cookie";
+import { StateContext } from "../context/StateContext";
+import { useContext } from "react";
+
+const cookie = new Cookie();
+
+export default function Task({ task, taskDeleted }) {
+  const { setSelectedTask } = useContext(StateContext);
+  const deleteTask = async () => {
+    await fetch(`https://nextjs-api-rest.herokuapp.com/api/tasks/${task.id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `JWT ${cookie.get("access_token")}`,
+      },
+    }).then((res) => {
+      if (res.status === 401) {
+        alert("JWT Token not valid");
+      }
+    });
+    taskDeleted();
+  };
+
+  return (
+    <div>
+      <span>{task.id}</span>
+      {" : "}
+      <Link href={`/tasks/${task.id}`}>
+        <span className="cursor-pointer text-white border-b border-gray-500 hover:bg-gray-600">
+          {task.title}
+        </span>
+      </Link>
+      <div className="float-right ml-20">
+        <svg
+          onClick={() => setSelectedTask(task)}
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-5 w-5 float-left cursor-pointer"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"
+          />
+        </svg>
+        <svg
+          onClick={deleteTask}
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-5 w-5 mr-2 cursor-pointer"
+          viewBox="0 0 20 20"
+          fill="currentColor"
+        >
+          <path
+            fillRule="evenodd"
+            d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
+            clipRule="evenodd"
+          />
+        </svg>
+      </div>
+    </div>
+  );
+}
